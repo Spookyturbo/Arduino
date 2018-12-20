f59b4883ae1111e00c0164ffc126b8667ef78c0a
Andy Riedlinger <technicalandyr@gmail.com>
2018-12-19 21:01:33 -0500

    Added in alarm and snooze functionality Reformatted a bit Added more comments

--- AlarmClock/AlarmClock.ino
+++ AlarmClock/AlarmClock.ino
@@ -6,6 +6,11 @@
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 RTC_DS3231 rtc;
 
+struct Time {
+  unsigned int hour: 5;
+  unsigned int minute: 6;
+};
+
 //Storing weekday info in the flash memory
 const char weekDay_0[] PROGMEM = "Sun";
 const char weekDay_1[] PROGMEM = "Mon";
@@ -40,7 +45,7 @@ const char month_9[] PROGMEM = "Oct";
 const char month_10[] PROGMEM = "Nov";
 const char month_11[] PROGMEM = "Dec";
 
-const char* const months[] PROGMEM = 
+const char * const months[] PROGMEM =
 {
   month_0,
   month_1,
@@ -56,17 +61,43 @@ const char* const months[] PROGMEM =
   month_11
 };
 
+const uint8_t sleepButtonPin = 3;
+const uint8_t sleepLightPin = 13;
+const uint8_t alarmOnPin = 4;
+const uint8_t alarmBuzzPin = 2;
+
+//120 seconds
+const uint8_t sleepLength = 120;
+bool sleepEnabled = false;
+bool previousSleepButtonState = false;
+
+const uint8_t alarmFrequency = 4500;
+const uint8_t alarmLength = 30;
+bool alarmBuzzing = false;
+struct Time alarmTime;
+
 void setup() {
   Serial.begin(9600);
+
+  //Initialise alarm
+  alarmTime.hour = 6;
+  alarmTime.minute = 20;
+  pinMode(alarmBuzzPin, OUTPUT);
+  pinMode(alarmOnPin, INPUT_PULLUP);
+
+  //Setup sleep button
+  pinMode(sleepButtonPin, INPUT_PULLUP);
+  pinMode(sleepLightPin, OUTPUT);
+  digitalWrite(sleepLightPin, LOW);
 
   //setting up the display
   lcd.begin(16, 2);
   //setting up the clock
-  if(!rtc.begin()) {
+  if (!rtc.begin()) {
     Serial.println(F("Clock could not properly initialise"));
   }
 
-  if(rtc.lostPower()) {
+  if (rtc.lostPower()) {
     Serial.println(F("Clock lost power, defaulting to compile time."));
     //Set time to compile time
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
@@ -76,17 +107,119 @@ void setup() {
 void loop() {
   printDate();
   printTime();
+  checkAlarm();
+
+  //Managing the sleep states
+    bool sleepButtonPressed = getSleepDown();
+    if(sleepButtonPressed && !sleepEnabled) {
+      enableSleep();
+    }
+    else if(sleepButtonPressed) {
+      disableSleep();
+    }
+
 }
 
+void checkAlarm() {
+  static unsigned long alarmStartTime = 0;
+  static unsigned long sleepStartTime = 0;
+  DateTime currentTime = rtc.now();
+
+  if (alarmBuzzing && isAlarmEnabled()) {
+    if (sleepEnabled) {
+      stopAlarm();
+      //Light up the sleep button
+      //Exit sleep mode
+      if (currentTime.unixtime() - sleepStartTime >= sleepLength) {
+        disableSleep();
+      }
+      //Will stop updating when alarm starts back up because sleep disables
+      alarmStartTime = currentTime.unixtime();
+    }
+    else {
+      soundAlarm();
+      //Exit alarm
+      if (currentTime.unixtime() - alarmStartTime >= alarmLength) {
+        alarmBuzzing = false;
+        stopAlarm();
+      }
+      //Sleep start time will stop updating when sleep is enabled
+      sleepStartTime = currentTime.unixtime();
+      //Unlight the sleep button
+    }
+  }
+  else { //Happens when the alarm finishes or when the alarm is turned off
+    alarmBuzzing = false;
+    disableSleep();
+    stopAlarm();
+  }
+
+  if (currentTime.hour() == alarmTime.hour && currentTime.minute() == alarmTime.minute && currentTime.second() == 0 && !sleepEnabled && isAlarmEnabled()) {
+    soundAlarm();
+    alarmBuzzing = true;
+    alarmStartTime = currentTime.unixtime();
+  }
+}
+
+void soundAlarm() {
+  tone(alarmBuzzPin, alarmFrequency);
+}
+
+//Stops the alarm buzzing
+void stopAlarm() {
+  noTone(alarmBuzzPin);
+}
+
+//turns on the light and only works while alarm is buzzing
+void enableSleep() {
+  if (alarmBuzzing) {
+    sleepEnabled = true;
+    digitalWrite(sleepLightPin, HIGH);
+  }
+}
+
+//Turns off the light and sets value
+void disableSleep() {
+  sleepEnabled = false;
+  digitalWrite(sleepLightPin, LOW);
+}
+
+//Returns the status of the alarm switch
+bool isAlarmEnabled() {
+  return !digitalRead(alarmOnPin);
+}
+
+/*
+   Returns true when button is first pressed
+   Button must be let go once to be pressed again
+*/
+bool getSleepDown() {
+  static bool previousState;
+  bool justPressed = false;
+  bool buttonPressed = !digitalRead(sleepButtonPin);
+  
+  if (!previousState && buttonPressed) {
+    justPressed = true;
+  }
+  previousState = buttonPressed;
+  return justPressed;
+}
+
+/*
+   Prints the date on the first line of the lcd
+   in the format: WWW, MMM DD
+   Example: Tue, Oct 27
+*/
 void printDate() {
   DateTime currentTime = rtc.now();
-  
+
+  //Retrieve month from flash memory
   char month[4];
   strcpy_P(month, (char*)pgm_read_word(&(months[currentTime.month() - 1])));
-  
+
   uint8_t day = currentTime.day();
-  uint8_t year = currentTime.year();
-  
+
+  //Retrieve weekDay from flash memory
   char weekDay[4];
   strcpy_P(weekDay, (char*)pgm_read_word(&(weekDays[currentTime.dayOfTheWeek()])));
 
@@ -96,9 +229,13 @@ void printDate() {
   lcd.print(month);
   lcd.print(" ");
   lcd.print(day);
-  
+
 }
 
+/*
+   Prints out the time in format HH:MM:SS P/A
+   On the second line of the LCD
+*/
 void printTime() {
   DateTime currentTime = rtc.now();
 
@@ -107,29 +244,29 @@ void printTime() {
   uint8_t second = currentTime.second();
 
   bool AM = true;
-  if(hour >= 12) {
+  if (hour >= 12) {
     AM = false;
-    if(hour >= 13)
-    hour -= 12;
+    if (hour >= 13)
+      hour -= 12;
   }
-  else if(hour == 0) {
+  else if (hour == 0) {
     hour = 12;
   }
-  
+
   lcd.setCursor(0, 1);
 
-  if(hour < 10)
+  if (hour < 10)
     lcd.print(0);
   lcd.print(hour);
   lcd.print(F(":"));
 
-  if(minute < 10)
+  if (minute < 10)
     lcd.print(0);
   lcd.print(minute);
   lcd.print(F(":"));
 
-  if(second < 10)
+  if (second < 10)
     lcd.print(0);
   lcd.print(second);
-  lcd.print((AM) ? F(" AM") : F(" PM"));
+  lcd.print((AM) ? F(" A") : F(" P"));
 }
